58318adbc0e98472e968725048bef3c1d5492a94
Jean-Marc Prud'homme <42589969+jmarc101@users.noreply.github.com>
2025-08-16 13:21:14 -0400

    wip

--- services/asr/grpc_service.py
+++ services/asr/grpc_service.py
@@ -2,7 +2,7 @@ import asyncio, tempfile, wave
 import grpc
 from voxa.speech.v1 import asr_pb2, asr_pb2_grpc, audio_pb2
 
-class AsrService(asr_pb2_grpc.ASRServicer):
+class AsrService(asr_pb2_grpc.AsrServicer):
     def __init__(self, engine):
         self.engine = engine
 
